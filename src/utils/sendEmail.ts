e058885c04408cb68c4bd7a15c1915bc47bd8a83
Jaber <jaberriyan357@gmail.com>
2025-09-19 22:12:28 +0600

    Updated Email sending

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,61 +1,66 @@
 import ejs from "ejs";
 import nodemailer from "nodemailer";
 import path from "path";
+import { fileURLToPath } from "url";
 import { envVars } from "../app/config/env";
 import AppError from "../app/errorHelpers/AppError";
 
+// Recreate __dirname in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const transporter = nodemailer.createTransport({
-    // port: envVars.EMAIL_SENDER.SMTP_PORT,
-    secure: true,
-    auth: {
-        user: envVars.EMAIL_SENDER.SMTP_USER,
-        pass: envVars.EMAIL_SENDER.SMTP_PASS
-    },
-    port: Number(envVars.EMAIL_SENDER.SMTP_PORT),
-    host: envVars.EMAIL_SENDER.SMTP_HOST
-})
+  secure: true,
+  auth: {
+    user: envVars.EMAIL_SENDER.SMTP_USER,
+    pass: envVars.EMAIL_SENDER.SMTP_PASS,
+  },
+  port: Number(envVars.EMAIL_SENDER.SMTP_PORT),
+  host: envVars.EMAIL_SENDER.SMTP_HOST,
+});
 
 interface SendEmailOptions {
-    to: string,
-    subject: string;
-    templateName: string;
-    templateData?: Record<string, any>
-    attachments?: {
-        filename: string,
-        content: Buffer | string,
-        contentType: string
-    }[]
+  to: string;
+  subject: string;
+  templateName: string;
+  templateData?: Record<string, any>;
+  attachments?: {
+    filename: string;
+    content: Buffer | string;
+    contentType: string;
+  }[];
 }
 
 export const sendEmail = async ({
-    to,
-    subject,
-    templateName,
-    templateData,
-    attachments
+  to,
+  subject,
+  templateName,
+  templateData,
+  attachments,
 }: SendEmailOptions) => {
-    try {
-        const templatePath = path.join(__dirname, `emailTemplates/${templateName}.ejs`)
+  try {
+    const templatePath = path.join(
+      __dirname,
+      `emailTemplates/${templateName}.ejs`
+    );
 
-        const html = await ejs.renderFile(templatePath, templateData)
+    const html = await ejs.renderFile(templatePath, templateData);
 
-        const info = await transporter.sendMail({
-            from: envVars.EMAIL_SENDER.SMTP_FROM,
-            to: to,
-            subject: subject,
-            html: html,
-            attachments: attachments?.map(attachment => ({
-                filename: attachment.filename,
-                content: attachment.content,
-                contentType: attachment.contentType
-            }))
-        })
+    const info = await transporter.sendMail({
+      from: envVars.EMAIL_SENDER.SMTP_FROM,
+      to,
+      subject,
+      html,
+      attachments: attachments?.map((attachment) => ({
+        filename: attachment.filename,
+        content: attachment.content,
+        contentType: attachment.contentType,
+      })),
+    });
 
-        console.log(`\u2709\uFE0F Email sent to ${to}: ${info.messageId}`);
-    
-    } catch (error: any) {
-        console.log("email sending error", error.message);
-        throw new AppError(401, "Email error")
-    }
-
-}
+    console.log(`✉️ Email sent to ${to}: ${info.messageId}`);
+  } catch (error: any) {
+    console.log("email sending error", error.message);
+    throw new AppError(401, "Email error");
+  }
+};
